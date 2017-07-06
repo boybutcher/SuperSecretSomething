339836169b3d5222d8bfae20cd210d3688b740e6
boybutcher <BYRONdeLARGE@yahoo.com>
2017-07-06 15:25:24 -0700

    apply flexboxon basic components

--- client/src/projects.js
+++ client/src/projects.js
@@ -12,7 +12,7 @@ class Projects extends Component {
 
   render() {
     return (
-      <div>
+      <div className='Projects'>
         {this.state.projects.map(project => 
           <ProjectTile />
         )}
