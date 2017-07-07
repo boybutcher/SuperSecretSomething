43c4423e6988712818ad4e05122e5ce502e950bd
boybutcher <BYRONdeLARGE@yahoo.com>
2017-07-07 13:53:43 -0700

    tileHover funciontality

--- client/src/projectTile.js
+++ client/src/projectTile.js
@@ -2,9 +2,18 @@ import React, {Component} from 'react';
 import './projectTile.css';
 
 class ProjectTile extends Component {
+  constructor(props) {
+    super(props);
+    this.tileHover = this.tileHover.bind(this);
+  }
+
+  tileHover() {
+    console.log('you\'re hovering over ', this.props.name);
+  }
+
   render() {
     return (
-      <div className='ProjectTile'>
+      <div className='ProjectTile' onMouseOver={this.tileHover}>
         <h1>{this.props.name}</h1>
       </div>
     )
