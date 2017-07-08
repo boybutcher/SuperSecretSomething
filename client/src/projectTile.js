2e97f99e78d4af7d345bacc32616f1ff9e31ac68
boybutcher <BYRONdeLARGE@yahoo.com>
2017-07-08 13:57:27 -0700

    general styling for projectTiles

--- client/src/projectTile.js
+++ client/src/projectTile.js
@@ -2,27 +2,6 @@ import React, {Component} from 'react';
 import './projectTile.css';
 
 class ProjectTile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hovering: false,
-    }
-    this.tileHover = this.tileHover.bind(this);
-    this.stopHover = this.stopHover.bind(this);
-  }
-
-  tileHover() {
-    this.setState({
-      hovering: true
-    })
-  }
-
-  stopHover() {
-    this.setState({
-      hovering: false
-    })
-  }
-
   render() {
     return (
       <div className='Tile'>
