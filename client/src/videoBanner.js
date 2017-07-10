d327de1bd3d2d0997fe49efbe96d49f60d632a85
boybutcher <BYRONdeLARGE@yahoo.com>
2017-07-10 15:35:44 -0700

    scroll to nav on click videoBanner

--- client/src/videoBanner.js
+++ client/src/videoBanner.js
@@ -2,10 +2,19 @@ import React, {Component} from 'react';
 import './videoBanner.css';
 
 class VideoBanner extends Component {
+  constructor(props) {
+    super(props);
+    this.scrollToNav = this.scrollToNav.bind(this);
+  }
+
+  scrollToNav() {
+    document.getElementsByClassName('NavBar')[0].scrollIntoView();
+  }
+
   render() {
     return (
       <div className='VideoBanner'>
-        <h1>King Gizzard and the Lizard Wizard</h1>
+        <h1 onClick={this.scrollToNav}>King Gizzard and the Lizard Wizard</h1>
       </div>
     );
   }
